9b95adbc61315b569ea405fa5c088a5f3e6294f4
Divin_Dass <projectwithdivin@gmail.com>
2024-01-27 15:59:59 +0000

    page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:currency_converter/converterPage.dart';
 
-
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override 
-  Widget build(BuildContext context){
-    return const Text(
-      'Hello World!',
-      textDirection: TextDirection.ltr,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: converterPage(),
     );
   }
 }
