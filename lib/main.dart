1cc987ac1fdc66c7e7aab03c40a85324aaa31b43
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2024-01-27 21:34:37 +0530

    widgets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
+import 'package:currency_converter/converterPage.dart';
 import 'package:flutter/material.dart';
-
 
 void main(){
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
-
   const MyApp({super.key});
 
-  @override 
+  @override
   Widget build(BuildContext context){
-    return const Text(
-      'Hello World!',
-      textDirection: TextDirection.ltr,
+    return const MaterialApp(
+      home: ConverterPage(),
     );
   }
 }
