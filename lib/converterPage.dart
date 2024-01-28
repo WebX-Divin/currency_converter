b9baea5f47c20c91fbaf978343046c4f13319fd3
Divin_Dass <projectwithdivin@gmail.com>
2024-01-28 16:45:07 +0000

    UI Updated

--- lib/converterPage.dart
+++ lib/converterPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ConverterPage extends StatelessWidget {
@@ -11,39 +12,62 @@ class ConverterPage extends StatelessWidget {
         width: 2.0,
         style: BorderStyle.solid,
       ),
-      borderRadius: BorderRadius.all(Radius.circular(40)),
+      borderRadius: BorderRadius.all(Radius.circular(7.5)),
     );
 
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 177, 98, 255),
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Currency Converter',
               style: TextStyle(
                 fontSize: 32,
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            TextField(
-              style: TextStyle(
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                hintText: 'Enter the amount in USD',
-                hintStyle: TextStyle(
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
                   color: Colors.black,
                 ),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                prefixIconColor: Colors.black,
-                filled: true,
-                fillColor: Colors.white,
-                focusedBorder: border,
-                enabledBorder: border,
+                decoration: InputDecoration(
+                  hintText: 'Enter the amount in USD',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
               ),
-              keyboardType: TextInputType.number,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('Button Clicked');
+                  }
+                },
+                style: const ButtonStyle(
+                  elevation: MaterialStatePropertyAll(15),
+                  backgroundColor: MaterialStatePropertyAll(Colors.black),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                  minimumSize:
+                      MaterialStatePropertyAll(Size(double.infinity, 50)),
+                ),
+                child: const Text('Convert'),
+              ),
             ),
           ],
         ),
