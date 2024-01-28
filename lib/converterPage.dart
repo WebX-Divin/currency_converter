c175042ef4f65014dfe9199892680e72ff5f0c21
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2024-01-28 15:09:07 +0530

    UI Upadte

--- lib/converterPage.dart
+++ lib/converterPage.dart
@@ -5,10 +5,51 @@ class ConverterPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+
+    const border = OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius:BorderRadius.all(Radius.circular(7.5)),
+                );
+
     return const Scaffold(
-        body: Center(
-          child: Text('Hello World!!!'),
+      backgroundColor: Color.fromARGB(255, 177, 98, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Currency Converter',
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.w700,
+            ),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Enter the amount in USD', 
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
