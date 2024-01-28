7394b6ec1e49e7fc0bcbb1854841648f3d68b837
Divin_Dass <projectwithdivin@gmail.com>
2024-01-28 09:38:11 +0000

    input field decoration added

--- lib/converterPage.dart
+++ lib/converterPage.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
 
-class converterPage extends StatelessWidget {
-  const converterPage({super.key});
+class ConverterPage extends StatelessWidget {
+  const ConverterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(40)),
+    );
+
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 177, 98, 255),
       body: Center(
-        child: Text('!!!!Hello World!!!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Currency Converter',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Enter the amount in USD',
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
       ),
     );
   }
